0a7d188f2c41e3ff56eaaf22e9eabc2ca4a76957
Your Name <you@example.com>
2021-01-22 09:49:43 -0600

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const dotenv = require(dotenv);
+const dotenv = require('dotenv');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
